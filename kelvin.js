afeb6c8b5fc156dc28b06ed19f675f8fa631964d
sveetyk <rotari.svetlana@gmail.com>
2021-06-23 23:53:38 +0300

    Kelvin

---
+++ kelvin.js
@@ -0,0 +1,16 @@
+//The variable type is const
+const kelvin = 293;
+
+//The converting og Kelvin in Celsius
+
+var celsius = kelvin - 273;
+
+//The converting of celsius in fahrenheit
+var fahrenheit = celsius * (9 / 5) + 32;
+
+//The round of the fahrenheit var
+Math.floor(fahrenheit);
+
+console.log('The temperature is ' + fahrenheit + ' degrees Fahrenheit');
+
+
